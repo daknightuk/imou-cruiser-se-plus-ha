8250c6c3032c722678520efdb073119549045953
daknightuk <84586585+daknightuk@users.noreply.github.com>
2026-09-20 22:56:37 +0100

    Add custom_components/imou_cruiser_se_plus/const.py

---
+++ custom_components/imou_cruiser_se_plus/const.py
@@ -0,0 +1,7 @@
+"""Constants for the Imou Cruiser SE Plus integration."""
+
+DOMAIN = "imou_cruiser_se_plus"
+PLATFORMS = ["button", "switch"]
+
+CONF_ONVIF_PORT = "onvif_port"
+DEFAULT_ONVIF_PORT = 80
